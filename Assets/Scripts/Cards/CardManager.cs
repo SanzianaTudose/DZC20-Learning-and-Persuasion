01a9c1fa32ed770182f5350020fab301d9a7e400
SanzianaTudose <tudosesanziana@gmail.com>
2021-12-04 19:23:08 +0100

    Added card drawing based on Expertise Areas
    + draw one single card from each area
    - Expertise Areas are always in the same order
    - all cards are redrawn (TODO: only redraw cards that have been used last time)
    
    TODO:
    * keep track of distributed cards

--- Assets/Scripts/Cards/CardManager.cs
+++ Assets/Scripts/Cards/CardManager.cs
@@ -1,32 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CardManager : MonoBehaviour {
     [SerializeField]
-    private List<Card> cards;
+    private List<Card> cardDeck;
 
+    // Holds a list of cards for each Expertise Area 
+    private Dictionary<Card.ExpertiseAreas, List<Card>> organizedCards;
     private List<GameObject> cardObjects;
 
-    void Awake() {
+    private void Awake() {
         SetPrivateVars();
         DistributeCards();
     }
 
+    private void Update() {
+        // TODO: remove this, it's just for debugging
+        if (Input.GetKeyDown("space")) {
+            Scene scene = SceneManager.GetActiveScene(); 
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+
     private void SetPrivateVars() {
+        organizedCards = new Dictionary<Card.ExpertiseAreas, List<Card>>();
+        foreach (Card card in cardDeck) {
+            if (!organizedCards.ContainsKey(card.expertiseArea))
+                organizedCards[card.expertiseArea] = new List<Card>();
+
+            organizedCards[card.expertiseArea].Add(card);
+        }
+
         cardObjects = new List<GameObject>();
         foreach (Transform child in transform)
             cardObjects.Add(child.gameObject);
     }
 
-    private void DistributeCards() { 
-        foreach (GameObject cardObject in cardObjects) {
-            CardDisplay cardDisplay= cardObject.GetComponent<CardDisplay>();
-            if (cardDisplay == null) {
+    private void DistributeCards() {
+        int curCardCount = 0;
+        foreach (Card.ExpertiseAreas expertise in organizedCards.Keys) {
+            GameObject cardObject = cardObjects[curCardCount++]; 
+            CardDisplay cardDisplay = cardObject.GetComponent<CardDisplay>();
+            if (cardDisplay == null)
                 Debug.LogError("CardManager.cs: no CardDisplay component found on child of CardsContainer.");
-            }
 
-            cardDisplay.SetCard(cards[Random.Range(0, cards.Count - 1)]);
+            Card curCard = organizedCards[expertise][Random.Range(0, organizedCards[expertise].Count)];
+            cardDisplay.SetCard(curCard);
         }
     }
 }
