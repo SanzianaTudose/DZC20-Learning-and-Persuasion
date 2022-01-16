93ad4f12ff34e442497accc0bf9649aaba440b97
Kolev <56160005+KolevVelyan@users.noreply.github.com>
2022-01-16 16:16:38 +0100

    OTB card rotates when clicked

--- Assets/Scripts/MainScene/Cards/OTBCardController.cs
+++ Assets/Scripts/MainScene/Cards/OTBCardController.cs
@@ -27,13 +27,31 @@ public class OTBCardController : MonoBehaviour
 
     // Called when Player clicks to draw an OTB Card
     public void OnBackClick() {
-        cardFront.SetActive(true);
-        cardBack.SetActive(false);
-
+        StartCoroutine(RotateOTBCard());
         hasOTB = true;
 
         // Choose random word from the OTB Decks
         otbWord = OTBCardDeck[Random.Range(0, OTBCardDeck.Count)].word;
         wordText.SetText(otbWord);
+    }
+
+    private IEnumerator RotateOTBCard()
+    {
+        for (float i = 0f; i <= 180f; i += 10f)
+        {
+            transform.rotation = Quaternion.Euler(0f, i, 0f);
+
+            if (i == 90f)
+            {
+                cardFront.SetActive(true);
+                cardBack.SetActive(false);
+            }
+            if (i > 90f)
+            {
+                cardFront.transform.rotation = Quaternion.Euler(0f, -1f * (180 - i), 0f);
+            }
+
+            yield return new WaitForSeconds(0.05f);
+        }
     }
 }
