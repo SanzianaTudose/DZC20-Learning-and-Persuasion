d2f3ec659f3336dba5199694b58b1e8f6dec1f80
SanzianaTudose <tudosesanziana@gmail.com>
2022-01-16 12:23:06 +0100

    Added OTB display

--- Assets/Scripts/MainScene/Cards/OTBCardController.cs
+++ Assets/Scripts/MainScene/Cards/OTBCardController.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class OTBCardController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [Header("Gameplay variables")]
+    [SerializeField] private List<Card> OTBCardDeck;
+
+    [Header("UI Fields")]
+    [SerializeField] private GameObject cardFront;
+    [SerializeField] private TMP_Text wordText;
+    [SerializeField] private GameObject cardBack;
+
+    private bool hasOTB = false;
+    private string OTBword = null;
+
+    public bool getHasOTB() {
+        return hasOTB;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // If OTB has been drawn, returns the word on the OTB otherwise returns null
+    public string getOTBWord() {
+        return OTBword;
+    }
+
+    // Called when Player clicks to draw an OTB Card
+    public void OnBackClick() {
+        cardFront.SetActive(true);
+        cardBack.SetActive(false);
+
+        // Choose random word from the OTB Decks
+        OTBword = OTBCardDeck[Random.Range(0, OTBCardDeck.Count)].word;
+        wordText.SetText(OTBword);
     }
 }
