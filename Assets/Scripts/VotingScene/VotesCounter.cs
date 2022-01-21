4b1a153bf5fb8888dff8920c0ddc819fcb6f6ace
Kolev <56160005+KolevVelyan@users.noreply.github.com>
2022-01-21 21:25:09 +0100

    Points are added only by master client + one card had a space in the word

--- Assets/Scripts/VotingScene/VotesCounter.cs
+++ Assets/Scripts/VotingScene/VotesCounter.cs
@@ -32,15 +32,13 @@ public class VotesCounter : MonoBehaviourPunCallbacks
             int newPoints = GetNumberOfPoints(votes, round, submittedBy);
             ansObjs[i].pointsThisRound = newPoints;
 
-            if (submittedBy == PhotonNetwork.LocalPlayer)
+            if (PhotonNetwork.LocalPlayer.IsMasterClient)
             {
                 // Add the points to the current points of the player
                 AddPointsToPlayer(submittedBy, newPoints);
             }
-
             newPointsArray[i] = newPoints;
         }
-
         transitionObj.StartRotatingAnswers();
     }
 
