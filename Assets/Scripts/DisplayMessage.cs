461e2ab545a47088b3e10cecf87610f612cc0c5b
Kolev <56160005+KolevVelyan@users.noreply.github.com>
2022-01-22 21:28:42 +0100

    Add sounds to DisplayMessage.

--- Assets/Scripts/DisplayMessage.cs
+++ Assets/Scripts/DisplayMessage.cs
@@ -7,8 +7,13 @@ public class DisplayMessage : MonoBehaviour
     public Message messagePrefab;
     private Canvas canvas;
 
+    private AudioSource source;
+
     public void DisplayNewMessage(string text)
     {
+        source = gameObject.GetComponent<AudioSource>();
+        source.PlayOneShot(source.clip);
+
         canvas = FindObjectOfType<Canvas>();
         Message newMessage = GameObject.Instantiate(messagePrefab, Vector3.zero, Quaternion.identity, canvas.transform);
         newMessage.SetMesssageText(text);
