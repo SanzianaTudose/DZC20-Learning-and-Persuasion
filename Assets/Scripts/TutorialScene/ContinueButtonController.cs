c51eddc62d8d16d8d18b95d46284d72a54d932d1
SanzianaTudose <tudosesanziana@gmail.com>
2022-01-23 20:48:54 +0100

    Added button to transition between ID and Game Tutorials

---
+++ Assets/Scripts/TutorialScene/ContinueButtonController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContinueButtonController : MonoBehaviour {
+    [SerializeField] private AudioSource profAudioSource;
+    [SerializeField] private GameObject idCanvas;
+    [SerializeField] private GameObject gameCanvas;
+    private Button continueButton;
+
+    void Start() {
+        continueButton = GetComponent<Button>();
+        continueButton.interactable = false;
+
+        idCanvas.SetActive(true);
+        gameCanvas.SetActive(false);
+    }
+
+    void Update() {
+        // Enable Continue button when audio is finished
+        if (!profAudioSource.isPlaying) {
+            continueButton.interactable = true;
+        }
+    }
+
+    public void OnClickContinue() {
+        // TODO: maybe add a nice transition
+        gameCanvas.SetActive(true);
+        idCanvas.SetActive(false);
+    }
+}
