73bf4e274001005eeb765ca46d10c8c10e93ca27
Kolev <56160005+KolevVelyan@users.noreply.github.com>
2022-01-24 19:16:58 +0100

    Made tutorial text auto scroll :)

---
+++ Assets/Scripts/TutorialScene/AutoScrollingText.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AutoScrollingText : MonoBehaviour
+{
+    public TMP_Text myText;
+    public float speed = 15;
+    private Vector3 textPosition;
+
+    void Start()
+    {
+        textPosition = myText.transform.position;
+    }
+
+    void Update()
+    {
+        if (myText.transform.localPosition.y < 1455f)
+        {
+            textPosition = new Vector3(textPosition.x, textPosition.y + 0.0001f * speed, textPosition.z);
+            myText.transform.position = textPosition;
+        }
+    }
+}
