57c043003634b7c50ed1bf3ea8535beca29c43a8
Kolev <56160005+KolevVelyan@users.noreply.github.com>
2022-01-25 20:19:44 +0100

    Added audio to after selecting squad

--- Assets/Scripts/SelectSquad/Cases/CaseIntroController.cs
+++ Assets/Scripts/SelectSquad/Cases/CaseIntroController.cs
@@ -8,7 +8,8 @@ using Photon.Pun;
  * Handles picking a Case for the input squad and transitioning from Squad voting to Case introduction. 
  * input: picked Squad
  */
-public class CaseIntroController : MonoBehaviour {
+public class CaseIntroController : MonoBehaviour
+{
     [Header("UI Fields")]
     [SerializeField] private GameObject squadVoteCanvas;
     [SerializeField] private TMP_Text squadText;
@@ -18,7 +19,25 @@ public class CaseIntroController : MonoBehaviour {
     [Header("Gameplay Variables")]
     [SerializeField] private List<Case> allCases;
 
-    public void startCaseIntro(string pickedSquad) {
+    private Dictionary<string, AudioClip> squadTooltips;
+
+    public AudioClip inclusiveDesign;
+    public AudioClip gamesAndPlay;
+    public AudioClip vitality;
+    public AudioClip transformingPractices;
+    public AudioClip crafting;
+    public AudioClip newFutures;
+    public AudioClip health;
+    public AudioClip sensoryMatters;
+    public AudioClip artifice;
+
+    public AudioSource audioSource;
+    private bool playAudioOnce = false;
+
+    public void startCaseIntro(string pickedSquad)
+    {
+        setUpDictionary();
+
         squadText.SetText(pickedSquad);
 
         // Transition UI
@@ -32,19 +51,57 @@ public class CaseIntroController : MonoBehaviour {
         // Only enable Continue button for MasterClient
         if (!PhotonNetwork.LocalPlayer.IsMasterClient)
             continueButton.SetActive(false);
-        
+
         if (PhotonNetwork.LocalPlayer.IsMasterClient)
             SetSquadAndCaseRoomProperties(pickedSquad, pickedCase);
+
+        startVoiceOver(pickedSquad);
     }
 
-    public void OnClickContinue() {
+    private void startVoiceOver(string squadName)
+    {
+        if (!playAudioOnce)
+        {
+            if (squadTooltips.ContainsKey(squadName))
+            {
+                audioSource.PlayOneShot(squadTooltips[squadName]);
+                playAudioOnce = true;
+                continueButton.SetActive(false);
+            }
+        } else
+        {
+            if (!audioSource.isPlaying)
+            {
+                continueButton.SetActive(true);
+            }
+        }
+    }
+
+    private void setUpDictionary()
+    {
+        squadTooltips = new Dictionary<string, AudioClip>();
+        squadTooltips.Add("Inclusive Design and Thoughtful Technology", inclusiveDesign);
+        squadTooltips.Add("Games And Play", gamesAndPlay);
+        squadTooltips.Add("Vitality", vitality);
+        squadTooltips.Add("Transforming Practices", transformingPractices);
+        squadTooltips.Add("Crafting Wearable Senses", crafting);
+        squadTooltips.Add("New Futures (connectivity in the home)", newFutures);
+        squadTooltips.Add("Health", health);
+        squadTooltips.Add("Sensory Matters (sustainable food systems)", sensoryMatters);
+        squadTooltips.Add("Artifice - Artificial Intelligence", artifice);
+    }
+
+    public void OnClickContinue()
+    {
         PhotonNetwork.LoadLevel("MainScene");
     }
 
-    private string PickCaseforSquad(string pickedSquad) {
+    private string PickCaseforSquad(string pickedSquad)
+    {
         List<Case> possibleCases = new List<Case>();
 
-        foreach (Case it in allCases) {
+        foreach (Case it in allCases)
+        {
             if (it.getSquadString() == pickedSquad)
                 possibleCases.Add(it);
         }
@@ -52,7 +109,8 @@ public class CaseIntroController : MonoBehaviour {
         Case pickedCase = possibleCases[Random.Range(0, possibleCases.Count - 1)];
         return pickedCase.caseText;
     }
-    private void SetSquadAndCaseRoomProperties(string pickedSquad, string pickedCase) {
+    private void SetSquadAndCaseRoomProperties(string pickedSquad, string pickedCase)
+    {
         ExitGames.Client.Photon.Hashtable roomProperties = new ExitGames.Client.Photon.Hashtable();
         roomProperties.Add("pickedSquad", pickedSquad);
         roomProperties.Add("pickedCase", pickedCase);
